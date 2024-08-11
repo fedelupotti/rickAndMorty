4203e9fe107f7f9446dbe711739a3601e0393381
Federico Lupotti <fedelupotti94@gmail.com>
2024-08-11 14:26:13 +0200

    Move some mock data to extensions

--- RickAndMortyTests/RickAndMortyTests.swift
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -19,8 +19,9 @@ final class RickAndMortyTests: XCTestCase {
     private lazy var cancellables = Set<AnyCancellable>()
     
     override func setUpWithError() throws {
-        sut = ViewModel(repository: Repository(apiService: MockAPIService()))
         sutRepository = Repository(apiService: MockAPIService())
+        sut = ViewModel(repository: sutRepository)
+        
     }
     
     override func tearDownWithError() throws {
@@ -29,17 +30,6 @@ final class RickAndMortyTests: XCTestCase {
         sutRepository = nil
         
         try super.tearDownWithError()
-    }
-    
-    func test_ViewModel_characterVariable_isCharacterType() {
-        //Given
-        let characterVariable = sut.characters
-        
-        //When
-        //Init already done
-        
-        //Then
-        XCTAssertTrue((characterVariable is [Character]))
     }
     
     func test_ViewModel_onRefresh_notExistOnInit() {
@@ -65,60 +55,15 @@ final class RickAndMortyTests: XCTestCase {
         XCTAssertTrue(sut.didCallFunction != nil)
     }
     
+    func test_ViewModel_onRefresh_isLoading() async {
+        
+        sut.onRefresh()
+        XCTAssertEqual(sut.isLoading, true)
+    }
+    
     func test_ViewModel_getAllIdEpisodesForCharacter_return51Episodes() {
         //Given
-        let character = Character(id: 1, episode: ["https://rickandmortyapi.com/api/episode/1",
-                                                   "https://rickandmortyapi.com/api/episode/2",
-                                                   "https://rickandmortyapi.com/api/episode/3",
-                                                   "https://rickandmortyapi.com/api/episode/4",
-                                                   "https://rickandmortyapi.com/api/episode/5",
-                                                   "https://rickandmortyapi.com/api/episode/6",
-                                                   "https://rickandmortyapi.com/api/episode/7",
-                                                   "https://rickandmortyapi.com/api/episode/8",
-                                                   "https://rickandmortyapi.com/api/episode/9",
-                                                   "https://rickandmortyapi.com/api/episode/10",
-                                                   "https://rickandmortyapi.com/api/episode/11",
-                                                   "https://rickandmortyapi.com/api/episode/12",
-                                                   "https://rickandmortyapi.com/api/episode/13",
-                                                   "https://rickandmortyapi.com/api/episode/14",
-                                                   "https://rickandmortyapi.com/api/episode/15",
-                                                   "https://rickandmortyapi.com/api/episode/16",
-                                                   "https://rickandmortyapi.com/api/episode/17",
-                                                   "https://rickandmortyapi.com/api/episode/18",
-                                                   "https://rickandmortyapi.com/api/episode/19",
-                                                   "https://rickandmortyapi.com/api/episode/20",
-                                                   "https://rickandmortyapi.com/api/episode/21",
-                                                   "https://rickandmortyapi.com/api/episode/22",
-                                                   "https://rickandmortyapi.com/api/episode/23",
-                                                   "https://rickandmortyapi.com/api/episode/24",
-                                                   "https://rickandmortyapi.com/api/episode/25",
-                                                   "https://rickandmortyapi.com/api/episode/26",
-                                                   "https://rickandmortyapi.com/api/episode/27",
-                                                   "https://rickandmortyapi.com/api/episode/28",
-                                                   "https://rickandmortyapi.com/api/episode/29",
-                                                   "https://rickandmortyapi.com/api/episode/30",
-                                                   "https://rickandmortyapi.com/api/episode/31",
-                                                   "https://rickandmortyapi.com/api/episode/32",
-                                                   "https://rickandmortyapi.com/api/episode/33",
-                                                   "https://rickandmortyapi.com/api/episode/34",
-                                                   "https://rickandmortyapi.com/api/episode/35",
-                                                   "https://rickandmortyapi.com/api/episode/36",
-                                                   "https://rickandmortyapi.com/api/episode/37",
-                                                   "https://rickandmortyapi.com/api/episode/38",
-                                                   "https://rickandmortyapi.com/api/episode/39",
-                                                   "https://rickandmortyapi.com/api/episode/40",
-                                                   "https://rickandmortyapi.com/api/episode/41",
-                                                   "https://rickandmortyapi.com/api/episode/42",
-                                                   "https://rickandmortyapi.com/api/episode/43",
-                                                   "https://rickandmortyapi.com/api/episode/44",
-                                                   "https://rickandmortyapi.com/api/episode/45",
-                                                   "https://rickandmortyapi.com/api/episode/46",
-                                                   "https://rickandmortyapi.com/api/episode/47",
-                                                   "https://rickandmortyapi.com/api/episode/48",
-                                                   "https://rickandmortyapi.com/api/episode/49",
-                                                   "https://rickandmortyapi.com/api/episode/50",
-                                                   "https://rickandmortyapi.com/api/episode/51"
-        ])
+        let character = Character.mock51EpisodesTest
         
         //When
         let idsEpisodes = sut.testGetAllIdEpisodesForCharacter(character)
@@ -127,51 +72,10 @@ final class RickAndMortyTests: XCTestCase {
         XCTAssert(idsEpisodes.count == 51)
     }
     
-    func test_getAllEpisodesForEpisodesId_take2EpisodesAmount() {
+    func test_getAllEpisodesForEpisodesId_take2Episodes() {
         //Given
         let episodesId = [1,2]
-        let episodes = [
-            Episode(id: 1, name: "Pilot", air_date: "December 2, 2013", episode: "S01E01", characters: [
-                "https://rickandmortyapi.com/api/character/1",
-                "https://rickandmortyapi.com/api/character/2",
-                "https://rickandmortyapi.com/api/character/35",
-                "https://rickandmortyapi.com/api/character/38",
-                "https://rickandmortyapi.com/api/character/62",
-                "https://rickandmortyapi.com/api/character/92",
-                "https://rickandmortyapi.com/api/character/127",
-                "https://rickandmortyapi.com/api/character/144",
-                "https://rickandmortyapi.com/api/character/158",
-                "https://rickandmortyapi.com/api/character/175",
-                "https://rickandmortyapi.com/api/character/179",
-                "https://rickandmortyapi.com/api/character/181",
-                "https://rickandmortyapi.com/api/character/239",
-                "https://rickandmortyapi.com/api/character/249",
-                "https://rickandmortyapi.com/api/character/271",
-                "https://rickandmortyapi.com/api/character/338",
-                "https://rickandmortyapi.com/api/character/394",
-                "https://rickandmortyapi.com/api/character/395",
-                "https://rickandmortyapi.com/api/character/435"
-            ]), Episode(id: 2, name: "Lawnmower Dog", air_date: "December 9, 2013", episode: "" , characters: ["https://rickandmortyapi.com/api/character/1",
-                "https://rickandmortyapi.com/api/character/2",
-                "https://rickandmortyapi.com/api/character/38",
-                "https://rickandmortyapi.com/api/character/46",
-                "https://rickandmortyapi.com/api/character/63",
-                "https://rickandmortyapi.com/api/character/80",
-                "https://rickandmortyapi.com/api/character/175",
-                "https://rickandmortyapi.com/api/character/221",
-                "https://rickandmortyapi.com/api/character/239",
-                "https://rickandmortyapi.com/api/character/246",
-                "https://rickandmortyapi.com/api/character/304",
-                "https://rickandmortyapi.com/api/character/305",
-                "https://rickandmortyapi.com/api/character/306",
-                "https://rickandmortyapi.com/api/character/329",
-                "https://rickandmortyapi.com/api/character/338",
-                "https://rickandmortyapi.com/api/character/396",
-                "https://rickandmortyapi.com/api/character/397",
-                "https://rickandmortyapi.com/api/character/398",
-                "https://rickandmortyapi.com/api/character/405"
-            ])
-        ]
+        let episodes = Episode.mock2EpisodesTest
         
         //When
         let episodesSelected = sut.testGetAllEpisodesForEpisodesId(episodesId: episodesId, episodes: episodes)
@@ -223,20 +127,20 @@ final class RickAndMortyTests: XCTestCase {
         XCTAssertTrue(characterOnInit.isEmpty)
     }
     
-    func test_Repository_fetchAllCharacters_atLeastOneCharacterAfterFetch() {
-        let exp = expectation(description: #function)
-        
-        //Given
-        sutRepository.didCallFetchAllCharacters = { exp.fulfill() }
-        sutRepository.fetchAllCharacters()
-        waitForExpectations(timeout: 1)
-        
-        //When
-        let characterOnInit = sutRepository.characters
-        
-        //Then
-        XCTAssertTrue(!characterOnInit.isEmpty)
-    }
+//    func test_Repository_fetchAllCharacters_atLeastOneCharacterAfterFetch() {
+//        let exp = expectation(description: #function)
+//        
+//        //Given
+//        sutRepository.didCallFetchAllCharacters = { exp.fulfill() }
+//        sutRepository.fetchAllCharacters()
+//        waitForExpectations(timeout: 1)
+//        
+//        //When
+//        let characterOnInit = sutRepository.characters
+//        
+//        //Then
+//        XCTAssertTrue(!characterOnInit.isEmpty)
+//    }
     
     func test_Repository_fetchAllCharacters_FirstCharacterId() async {
         
